50b1c0172c445b2de7cd0e8a3ca02e5df1fcf8e6
rstarcic <roberta.starcic589@gmail.com>
2023-03-29 19:10:26 +0200

    commit 2

--- KZ01_GrupaB.js
+++ KZ01_GrupaB.js
@@ -2,18 +2,40 @@
 //Napisi funkciju koja ispisuje neparne brojeve od 0 do 100
 function neparniBrojevi () {
     for(let broj = 0; broj <= 100; broj++) {
-        if(broj % 3 == 0)
+        if(broj % 2 != 0) {
+            console.log(broj);
+        }
     }
 }
+neparniBrojevi();
 //2 (3 BODA)
 //Napisi funkciju koja koristi switch selekciju za pretvaranje numerickog unosa u godisnje u doba. Za neispravan unos ispisi prikladnu poruku.
 //INPUT: 1; OUTPUT: "Jesen";
 //INPUT: 3; OUTPUT: "Ljeto"; 
-
+function GodisnjeDoba (number) {
+    switch(number) {
+        case 1: 
+            console.log("Jesen");
+            break;
+        case 2:
+            console.log("Proljeće");
+            break;
+        case 3:
+            console.log("Ljeto");
+            break;
+        case 4: 
+            console.log("Zima");
+            break;
+    }
+}
+GodisnjeDoba(4);
 //3 (4 BODA)
 //Napisi funkciju koja okrece uneseni string i broji charactere uzimajuci u obzir prazna mjesta (eng. whitespaces)
 //INPUT: "Pisanje zadataka nije jednostavan posao"
 //OUTPUT: ("oasop navatsondej ejin akatadaz ejnasiP", 39)
+function reversedString (str) {
+    
+}
 
 //4 (4 BODA)
 //Napisi funkciju koja odreduje najdulju vrijednostu u danom objektu
@@ -28,6 +50,16 @@ const peopleYouShouldKnow = {
 //INPUT: somePeople
 //OUTPUT: "Stroustrup"
 
+function NajduljaVrijednost (somePeople) {
+    let najdulje = 0;
+    for(key in somePeople) {
+        if(somePeople[key].length > najdulje) {
+            najdulje = somePeople[key];
+        }
+    }
+    return najdulje;
+}
+NajduljaVrijednost(Donald);
 //5 (4 BODA)
 //Napisi funkciju u obliku function expressiona koja vraca neproste brojeve
 //INPUT: [3, 4, 5, 8, 11]
@@ -64,3 +96,4 @@ const someEvents = [
 ];
 //INPUT: (someStates, 48.5)
 //OUTPUT: [{stateName: "Grcka", measuredTemperature: 48.0}]
+function tempDrzava () {}
